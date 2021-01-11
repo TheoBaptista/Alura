484d5eb01c15b191a44b9f2c13ed74fb28618f20
@TheoBaptista <theoalfonso78@gmail.com>
2021-01-11 16:42:35 -0300

    Aplicando o pattern State

--- Design Patterns/loja/src/br/com/alura/loja/orcamento/Orcamento.java
+++ Design Patterns/loja/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -1,22 +1,32 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.Situacao;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
 
     private BigDecimal valor;
 
-    private String situação;
+    private Situacao situacao;
 
     private int quantidadeDeItens;
 
     public Orcamento(BigDecimal valor, int quantidadeDeItens) {
         this.valor = valor;
         this.quantidadeDeItens = quantidadeDeItens;
-        
+        this.situacao = new EmAnalise();
+
     }
 
+    public void setSituacao(Situacao situacao) {
+        this.situacao = situacao;
+    }
 
+    public Situacao getSituacao() {
+        return situacao;
+    }
 
     public BigDecimal getValor() {
         return valor;
@@ -35,12 +45,16 @@ public class Orcamento {
     }
 
     public void aplicarDescontoExtra(){
-        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
-                if(situação.equals("ANALISE")){
-                    valorDoDescontoExtra = new BigDecimal("0.05");
-                }else if(situação.equals("APROVADO")){
-                    valorDoDescontoExtra = new BigDecimal("0.02");
-                }
+        BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDoDescontoExtra(this);
                 this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+    public void aprovar(){
+        this.situacao.aprovar(this);
+    }
+    public void reprovar(){
+        this.situacao.reprovar(this);
+    }
+    public void finalizar(){
+        this.situacao.finalizar(this);
     }
 }
