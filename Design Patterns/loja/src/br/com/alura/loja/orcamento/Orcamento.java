6b2a91426640bf1d2fe6ea274811c6b74efa49ac
@TheoBaptista <theoalfonso78@gmail.com>
2021-01-11 15:35:41 -0300

    Criando metodo para desconto extra.

--- Design Patterns/loja/src/br/com/alura/loja/orcamento/Orcamento.java
+++ Design Patterns/loja/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -6,11 +6,14 @@ public class Orcamento {
 
     private BigDecimal valor;
 
+    private String situação;
+
     private int quantidadeDeItens;
 
     public Orcamento(BigDecimal valor, int quantidadeDeItens) {
         this.valor = valor;
         this.quantidadeDeItens = quantidadeDeItens;
+        
     }
 
 
@@ -29,5 +32,15 @@ public class Orcamento {
 
     public void setQuantidadeDeItens(int quantidadeDeItens) {
         this.quantidadeDeItens = quantidadeDeItens;
+    }
+
+    public void aplicarDescontoExtra(){
+        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+                if(situação.equals("ANALISE")){
+                    valorDoDescontoExtra = new BigDecimal("0.05");
+                }else if(situação.equals("APROVADO")){
+                    valorDoDescontoExtra = new BigDecimal("0.02");
+                }
+                this.valor = this.valor.subtract(valorDoDescontoExtra);
     }
 }
